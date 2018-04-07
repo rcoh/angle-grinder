8f8d26420cbdca51176a69f136a0c34ec071ab4e
Russell Cohen <russell.r.cohen@gmail.com>
2018-04-06 17:49:50 -0700

    Switch to Quicli!

--- src/bin/agrind.rs
+++ src/bin/agrind.rs
@@ -1,51 +1,37 @@
 extern crate ag;
-extern crate clap;
-use ag::pipeline;
-use clap::{App, Arg};
+#[macro_use]
+extern crate quicli;
+
+use ag::pipeline::Pipeline;
+use quicli::prelude::*;
 use std::fs::File;
 use std::io;
 use std::io::BufReader;
-fn main() {
-    ::std::process::exit(match run_agrind() {
-        Ok(_) => 0,
-        Err(_) => 1,
-    });
+
+#[derive(Debug, StructOpt)]
+struct Cli {
+    /// Query
+    query: String,
+    // Add a positional argument that the user has to supply:
+    /// Optionally reads from a file instead of Stdin
+    #[structopt(long = "file", short = "f")]
+    file: Option<String>,
+    /// Pass many times for more log output
+    #[structopt(long = "verbose", short = "v", parse(from_occurrences))]
+    verbosity: u8,
 }
-fn run_agrind() -> Result<(), ()> {
-    let matches = App::new("angle-grinder")
-        .version(env!("CARGO_PKG_VERSION"))
-        .author("Russell Cohen <rcoh@rcoh.me>")
-        .about("Slice and dice log files on the command line")
-        .help("For more details + docs, see https://github.com/rcoh/angle-grinder")
-        .arg(
-            Arg::with_name("query")
-                .help("ag query string eg: `...`")
-                .required(true)
-                .index(1),
-        )
-        .arg(Arg::from_usage(
-            "-f --file=[FILE] 'Optionally reads from a file instead of Stdin'",
-        ))
-        .get_matches();
-    let query_str = matches.value_of("query").unwrap();
-    match pipeline::Pipeline::new(query_str) {
-        Result::Ok(pipeline) => match matches.value_of("file") {
-            None => {
-                let stdin = io::stdin();
-                let locked = stdin.lock();
-                pipeline.process(locked);
-                Ok(())
-            }
-            Some(file) => {
-                let f = File::open(file).unwrap();
-                let f = BufReader::new(f);
-                pipeline.process(f);
-                Ok(())
-            }
-        },
-        Result::Err(e) => {
-            eprintln!("{}", e);
-            Err(())
+
+main!(|args: Cli, log_level: verbosity| {
+    let pipeline = Pipeline::new(&args.query)?;
+    match args.file {
+        Some(file_name) => {
+            let f = File::open(file_name)?;
+            pipeline.process(BufReader::new(f))
+        }
+        None => {
+            let stdin = io::stdin();
+            let locked = stdin.lock();
+            pipeline.process(locked)
         }
     }
-}
+});
