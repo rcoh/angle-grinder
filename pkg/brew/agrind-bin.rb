c2b5f3025486251bab545c384ac8dd43a1ed9792
Russell Cohen <russell.r.cohen@gmail.com>
2018-04-09 11:01:46 -0700

    Update brew package

--- pkg/brew/agrind-bin.rb
+++ pkg/brew/agrind-bin.rb
@@ -1,14 +1,14 @@
 class AgrindBin < Formula
-  version 'v0.7.1'
+  version 'v0.7.3'
   desc "Slice and dice log files on the command line"
   homepage "https://github.com/rcoh/angle-grinder"
 
   if OS.mac?
       url "https://github.com/rcoh/angle-grinder/releases/download/#{version}/angle_grinder-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "be280ed87b044438fe20a87ceb2be96930ccab7ebf7b1ac80385abd8edb19b53"
+      sha256 "d0682656294bc5d764d4110ae93add1442f6420bff49dcc4d49cb635950014fb
   elsif OS.linux?
       url "https://github.com/rcoh/angle-grinder/releases/download/#{version}/angle_grinder-#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "5ce30c62d090e8ca0c71d5a3f10a8ff46363d67d2c651981cb8d18a7d93dcf77"
+      sha256 "6dc5438443e653e5d893c6858d0f9279205728314292636c7b5b18706a9aa759
   end
 
 
