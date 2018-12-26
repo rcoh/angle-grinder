a4075fc17719b73c8c74323b43c42dd6449a1815
Russell Cohen <russell.r.cohen@gmail.com>
2018-12-26 10:56:30 -0800

    Fixes

--- src/bin/agrind.rs
+++ src/bin/agrind.rs
@@ -1,27 +1,29 @@
 extern crate ag;
-#[macro_use]
-extern crate quicli;
-
+#[macro_use] extern crate quicli;
+#[macro_use] extern crate structopt;
+use quicli::prelude::*;
 use ag::pipeline::Pipeline;
 use quicli::prelude::*;
 use std::fs::File;
 use std::io;
 use std::io::BufReader;
+use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
-#[structopt(after_help = "For more details + docs, see https://github.com/rcoh/angle-grinder")]
+//#[structopt(after_help = "For more details + docs, see https://github.com/rcoh/angle-grinder")]
 struct Cli {
     /// The query
     query: String,
     /// Optionally reads from a file instead of Stdin
     #[structopt(long = "file", short = "f")]
     file: Option<String>,
-    /// Pass many times for more log output
-    #[structopt(long = "verbose", short = "v", parse(from_occurrences))]
-    verbosity: u8,
+    #[structopt(flatten)]
+    verbosity: Verbosity,
 }
 
-main!(|args: Cli, log_level: verbosity| {
+fn main() -> CliResult {
+    let args = Cli::from_args();
+    args.verbosity.setup_env_logger("agrind")?;
     let pipeline = Pipeline::new(&args.query)?;
     match args.file {
         Some(file_name) => {
@@ -33,5 +35,6 @@ main!(|args: Cli, log_level: verbosity| {
             let locked = stdin.lock();
             pipeline.process(locked)
         }
-    }
-});
+    };
+    Ok(())
+}
