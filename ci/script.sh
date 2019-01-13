c00aa6b0c81bbbc3bad136bdba9c94a0d8d64930
Russell Cohen <russell.r.cohen@gmail.com>
2019-01-12 17:51:38 -0800

    Stop using cross for builds

--- ci/script.sh
+++ ci/script.sh
@@ -1,21 +1,19 @@
+#!/bin/bash
 # This script takes care of testing your crate
 
 set -ex
 
-# TODO This is the "test phase", tweak it as you see fit
 main() {
-    cross build --target $TARGET
-    cross build --target $TARGET --release
-
+    cargo build
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
+    cargo test
+    cargo run -- --help
 
-    cross test --target $TARGET
-    cross test --target $TARGET --release
-
-    cross run --target $TARGET -- --help
-    cross run --target $TARGET --release -- --help
+    if [ $TEST_RELEASE ]; then
+        cargo test --release
+    fi
 }
 
 # we don't run the "test phase" when doing deploys
