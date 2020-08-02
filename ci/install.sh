7aae8a9c0fe26970bd640c2d5508102aeeac3be6
Russell Cohen <russell.r.cohen@gmail.com>
2020-08-02 17:33:49 -0400

    Bump version

--- ci/install.sh
+++ ci/install.sh
@@ -5,6 +5,7 @@ main() {
     if [ $TRAVIS_OS_NAME = linux ]; then
         target=x86_64-unknown-linux-musl
         sort=sort
+        apt-get install -y libssl-dev
     else
         target=x86_64-apple-darwin
         sort=gsort  # for `sort --sort-version`, from brew's coreutils.
